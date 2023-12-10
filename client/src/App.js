27a733806855ef2f3b807c3e9915f4f8d1ec8e3f
Enes Ceylan <63819815+MedusaCollins@users.noreply.github.com>
2023-12-10 14:47:35 +0300

    Add socket.io for real-time communication

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,53 @@
-export default function App() {
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const socket = io("http://localhost:3001");
+    setSocket(socket);
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message !== "" && socket) {
+      socket.emit("chat message", message);
+      setMessage("");
+    }
+  };
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+    <div className="flex flex-col h-full p-3">
+      <div className="overflow-auto mb-3">
+        {messages.map((message, index) => (
+          <div key={index} className="p-1 rounded bg-blue-500 text-white my-1">
+            {message}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={sendMessage} className="flex">
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="flex-grow rounded-l px-3 py-2 outline-none"
+        />
+        <button type="submit" className="px-3 py-2 bg-blue-500 text-white rounded-r">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,38 @@
+import Express from "express";
+import { Server } from "socket.io";
+import http from "http";
+import cors from "cors";
 
-const express = require('express');
-const app = express();
+const app = Express();
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
-// Your code goes here
-const port = 3001
-app.listen(port, () => {
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+const port = 3001;
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("chat message", (msg) => {
+  console.log(`Message: ${msg}`);
+  io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+  console.log("A user disconnected");
+  });
 });
