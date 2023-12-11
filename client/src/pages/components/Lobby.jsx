e824b184c55b31aae87dbba9e021947e63d61fdf
Enes Ceylan <63819815+MedusaCollins@users.noreply.github.com>
2023-12-12 00:48:50 +0300

    Refactor Lobby component and add tags and maxPlayer property

--- client/src/pages/components/Lobby.jsx
+++ client/src/pages/components/Lobby.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import { useState } from 'react'
 
 const Lobby = () => {
-    const [rooms, setRooms] = useState([{name: 'room1', players: [{name: 'Medusa Collins', email: 'collinsmedusa@gmail.com', imageUrl: 'https://lh3.googleusercontent.com/a/ACg8ocJUsNIwm70oAlbkR3-J-XJ4RoN2ySL-YK_hCqp2C4Wzmg=s96-c'}, {name: 'Medusa Collins', email: 'collinsmedusa@gmail.com', imageUrl: 'https://lh3.googleusercontent.com/a/ACg8ocJUsNIwm70oAlbkR3-J-XJ4RoN2ySL-YK_hCqp2C4Wzmg=s96-c'}], host: {name: 'Medusa Collins', email: 'collinsmedusa@gmail.com', imageUrl: 'https://lh3.googleusercontent.com/a/ACg8ocJUsNIwm70oAlbkR3-J-XJ4RoN2ySL-YK_hCqp2C4Wzmg=s96-c'}, mode: 'classic', status: 'waiting', }, {name: 'room1', players: [{name: 'Medusa Collins', email: 'collinsmedusa@gmail.com', imageUrl: 'https://lh3.googleusercontent.com/a/ACg8ocJUsNIwm70oAlbkR3-J-XJ4RoN2ySL-YK_hCqp2C4Wzmg=s96-c'}, {name: 'Medusa Collins', email: 'collinsmedusa@gmail.com', imageUrl: 'https://lh3.googleusercontent.com/a/ACg8ocJUsNIwm70oAlbkR3-J-XJ4RoN2ySL-YK_hCqp2C4Wzmg=s96-c'}], host: {name: 'Medusa Collins', email: 'collinsmedusa@gmail.com', imageUrl: 'https://lh3.googleusercontent.com/a/ACg8ocJUsNIwm70oAlbkR3-J-XJ4RoN2ySL-YK_hCqp2C4Wzmg=s96-c'}, mode: 'classic', status: 'waiting', }])
+    const [rooms, setRooms] = useState([{
+            name: 'room1',
+            tags: ['power', 'classic'],
+            players: [{name: 'Medusa Collins', email: 'collinsmedusa@gmail.com', imageUrl: 'https://lh3.googleusercontent.com/a/ACg8ocJUsNIwm70oAlbkR3-J-XJ4RoN2ySL-YK_hCqp2C4Wzmg=s96-c'}, {name: 'Medusa Collins', email: 'collinsmedusa@gmail.com', imageUrl: 'https://lh3.googleusercontent.com/a/ACg8ocJUsNIwm70oAlbkR3-J-XJ4RoN2ySL-YK_hCqp2C4Wzmg=s96-c'}],
+            host: {name: 'Medusa Collins', email: 'collinsmedusa@gmail.com', imageUrl: 'https://lh3.googleusercontent.com/a/ACg8ocJUsNIwm70oAlbkR3-J-XJ4RoN2ySL-YK_hCqp2C4Wzmg=s96-c'}, 
+            mode: 'classic tak-on', 
+            status: 'waiting', 
+            maxPlayer: 10}])
     console.log(rooms)
   return (
     <div className='p-12 space-y-12 text-white'>
@@ -11,7 +18,7 @@ const Lobby = () => {
                 <div className='grid col-span-1'>
                     <h1 className='text-2xl'>{room.mode}</h1>
                     <div className='flex gap-5'>
-                        <p>Players: {room.players.length}</p>
+                        <p>Players: {room.players.length+1} / {room.maxPlayer}</p>
                         <h5>Status: {room.status}</h5>
                     </div>
                 </div>
@@ -20,11 +27,20 @@ const Lobby = () => {
                         <img src={room.host.imageUrl} alt='Host' className='border border-red-500'/>
                         <div className='justify-between flex flex-col'>
                             <h2>{room.name}</h2>
-                            <h2 className='flex gap-5'>{room.players.map((player, index) => (
-                                <div>
+                            <div className='flex'>
+                                {room.tags.map((tag) => (
+                                    <div className='rounded-full bg-yellow-300 text-black p-1 m-1 text-sm'>#{tag}</div>
+                                    ))}
+                            </div>
+                            <div className='flex gap-5'>
+                                <h2 className='flex gap-5'>{room.players.map((player, index) => (
                                     <img src={player.imageUrl} alt={player.name}  className='w-12 border border-red-500'/>
-                                </div>
-                            ))}</h2>
+                                ))}
+                                </h2>
+                                {Array(room.maxPlayer - room.players.length - 1).fill().map((_, index) => (
+                                    <div className='border border-green-500 w-12 h-12'></div>
+                                ))}
+                        </div>
                         </div>
                     </div>
                 </div>
--- client/src/pages/components/LobbyModule.jsx
+++ client/src/pages/components/LobbyModule.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LobbyModule = () => {
   return (
     <div className='text-white flex gap-5 items-center mx-auto place-content-center'>
-        <button className='p-2 bg-blue-500 hover:bg-blue-600 rounded-lg'>New Game</button>
+        <button onClick={console.log("Yeni oyun")} className='p-2 bg-blue-500 hover:bg-blue-600 rounded-lg'>New Game</button>
         <button className='p-2 bg-green-500 hover:bg-green-600 rounded-lg'>Quick Join</button>
     </div>
   )
