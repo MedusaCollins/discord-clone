9f6d12e6d9578af0bd2dae4643150e9bd5c98403
Enes Ceylan <63819815+MedusaCollins@users.noreply.github.com>
2023-12-18 20:27:39 +0300

    Add popup menu to Channels component

--- client/src/pages/components/Channels.jsx
+++ client/src/pages/components/Channels.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown, faHashtag, faVolumeHigh, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faHashtag, faVolumeHigh, faArrowRightFromBracket, faX } from '@fortawesome/free-solid-svg-icons'
 
 const Channels = (params) => {
   const {selected, selectedServer, setSelected, user} = params
+  const [popup, setPopup] = React.useState(false)
   return (
     <div className="w-[10%] h-screen bg-black-200 text-white flex flex-col relative justify-between">
       {selectedServer.channels != null &&
         <div>
-          <p className='justify-between items-center flex px-5 py-2 hover:bg-black-hover cursor-pointer transition-all'>{selectedServer.name} 
-            <span className='text-sm'><FontAwesomeIcon icon={faAngleDown} /></span>
-          </p>
-
+          <button onClick={() => popup ? setPopup(false): setPopup(true)} className='justify-between items-center flex px-5 py-2 w-full hover:bg-black-hover cursor-pointer transition-all'>{selectedServer.name} 
+            <span className='text-sm'><FontAwesomeIcon icon={popup ? faX : faAngleDown} /></span>
+          </button>
+          {popup && (
+            <div className='bg-black-400 absolute w-[90%] m-2 rounded-md'>
+              <ul className='p-2'>
+                <li className="hover:bg-blue cursor-pointer text-gray-100 hover:text-white p-1 text-sm">Server Settings</li>
+                <li className="hover:bg-blue cursor-pointer text-blue hover:text-white p-1 text-sm">Invite People</li>
+                <li className="hover:bg-red-500 cursor-pointer text-red-500 hover:text-white p-1 text-sm">Leave Server</li>
+              </ul>
+            </div>
+          )}
           {selectedServer.channels.map((channel,index) => (
             <div key={index} onClick={() => (selected.channelID!==channel._id ? setSelected({...selected, channelID:channel._id}) : null)} 
             className={`flex items-center h-7 mx-2 my-1 px-2 cursor-pointer rounded-md transition-all
