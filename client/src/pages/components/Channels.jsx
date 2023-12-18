bfef1171338622c5d42626dc1516d7aa255e9fac
Enes Ceylan <63819815+MedusaCollins@users.noreply.github.com>
2023-12-18 21:25:35 +0300

    Add invite link popup

--- client/src/pages/components/Channels.jsx
+++ client/src/pages/components/Channels.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown, faHashtag, faVolumeHigh, faArrowRightFromBracket, faX } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faHashtag, faVolumeHigh, faArrowRightFromBracket, faX, faGear, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 const Channels = (params) => {
   const {selected, selectedServer, setSelected, user} = params
-  const [popup, setPopup] = React.useState(false)
+  const [popup, setPopup] = React.useState({
+    serverInfo: false,
+    invite: false,
+    leave: false
+  })
+  const [text, setText] = React.useState('Copy')
+
+  function invite(){
+    navigator.clipboard.writeText(selectedServer._id)
+    setText('Copied')
+    setTimeout(() => setText('Copy'), 1000)
+  }
   return (
     <div className="w-[10%] h-screen bg-black-200 text-white flex flex-col relative justify-between">
       {selectedServer.channels != null &&
         <div>
-          <button onClick={() => popup ? setPopup(false): setPopup(true)} className='justify-between items-center flex px-5 py-2 w-full hover:bg-black-hover cursor-pointer transition-all'>{selectedServer.name} 
-            <span className='text-sm'><FontAwesomeIcon icon={popup ? faX : faAngleDown} /></span>
+          <button onClick={() => popup.serverInfo ? setPopup({...popup, serverInfo:false}): setPopup({...popup, serverInfo:true})} className='justify-between items-center flex px-5 py-2 w-full hover:bg-black-hover cursor-pointer transition-all'>{selectedServer.name} 
+            <span className='text-sm text-gray-100'><FontAwesomeIcon icon={popup.serverInfo ? faX : faAngleDown} /></span>
           </button>
-          {popup && (
+          {popup.serverInfo && (
             <div className='bg-black-400 absolute w-[90%] m-2 rounded-md'>
               <ul className='p-2'>
-                <li className="hover:bg-blue cursor-pointer text-gray-100 hover:text-white p-1 text-sm">Server Settings</li>
-                <li className="hover:bg-blue cursor-pointer text-blue hover:text-white p-1 text-sm">Invite People</li>
-                <li className="hover:bg-red-500 cursor-pointer text-red-500 hover:text-white p-1 text-sm">Leave Server</li>
+                <li className="hover:bg-blue cursor-pointer text-gray-100 hover:text-white p-1 px-2 text-sm justify-between flex items-center rounded-sm">Server Settings <FontAwesomeIcon icon={faGear} /></li>
+                <li onClick={()=> setPopup({...popup, invite:true})} className="hover:bg-blue-50 cursor-pointer text-blue-50 hover:text-white p-1 px-2 text-sm justify-between flex items-center rounded-sm">Invite People <FontAwesomeIcon icon={faUserPlus} /></li>
+                <li className="hover:bg-red-500 cursor-pointer text-red-500 hover:text-white p-1 px-2 text-sm justify-between flex items-center rounded-sm">Leave Server <FontAwesomeIcon icon={faArrowRightFromBracket} /></li>
               </ul>
             </div>
           )}
@@ -40,6 +51,27 @@ const Channels = (params) => {
         <p className='text-sm'>{user.name}</p>
         <button onClick={()=> params.setLogin(false)} className='right-2 absolute'><FontAwesomeIcon icon={faArrowRightFromBracket} /></button>
       </div>
+
+      {popup.invite && (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#000000] bg-opacity-60" onClick={() => setPopup({...popup, invite: false})}>
+        <div className="relative bg-[#313338] rounded-xl shadow-md mx-5" onClick={(e) => e.stopPropagation()}>
+          <div className="text-center sm:w-96">
+              <div className='p-4'>
+                <p className='text-lg text-white font-semibold'>Invite friends to {selectedServer.name}</p>
+                <div className='flex flex-col text-left mt-5'>
+                  <label htmlFor="Server name" className="block text-ssm font-bold leading-6 text-gray-100">
+                    SEND SERVER ID TO FRIENDS TO INVITE THEM
+                  </label>
+                  <div className='flex items-center justify-between bg-black-400 p-1'>
+                    <input type="text" value={selectedServer._id} readOnly className="px-1 text-sm block w-full ring-0 outline-none bg-black-400 text-gray-100"/>
+                    <button className={`text-white text-ssm px-5 py-2 rounded-sm bg-${text==="Copy" ?'blue-50': 'green-600'} hover:bg-${text==="Copy" ?'[#454fc0]': 'bg-green-400'}`} onClick={() => invite()}>{text}</button>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div> 
+      )}
     </div>
   )
 }
